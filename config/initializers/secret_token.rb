3fceac2417f049968520ebbc7d0c0199ce709a36
Samu Voutilainen <smar@smar.fi>
2014-01-04 17:42:50 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shinkaya::Application.config.secret_key_base = 'b383a063df94b2db356ad794c885f19b870be4a17b583396abbfb84050e5cf92bd9ddb5a28b5da8dafdde818deb5bb8d276e167a7f7a0bed31f0099ff80a4a92'
